e06062173d95e832368267f8f1084c23f7371554
DeGraciaMathieu <degracia.mathieu@gmail.com>
2024-02-05 09:03:43 +0100

    Added missing get method on TrackedFileRepository contract

--- app/Domain/Contracts/Repositories/TrackedFileRepository.php
+++ app/Domain/Contracts/Repositories/TrackedFileRepository.php
@@ -2,9 +2,15 @@
 
 namespace App\Domain\Contracts\Repositories;
 
+use App\Domain\Entities\TrackedFile;
 use App\Domain\Aggregators\TrackedFileAggregator;
 
 interface TrackedFileRepository
 {
+    /**
+     * @return array<TrackedFile>
+     */
+    public function get(): array;
+    
     public function store(TrackedFileAggregator $aggregator): void;
 }
